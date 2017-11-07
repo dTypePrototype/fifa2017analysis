521427c41cbee18bc40ad6c770ed868a079b0e00
Dewang Nautiyal <dewangnautiyalddn@gmail.com>
2017-11-07 22:14:14 +0530

    My Files

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <QMediaPlayer>
+#include <QVideoWidget>
+#include <QThread>
+
+class Sleeper : public QThread
+{
+public:
+    static void sleep(unsigned long secs){QThread::sleep(secs);}
+};
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QMediaPlayer* player =new QMediaPlayer;
+    QVideoWidget*vw =new QVideoWidget;
+    player->setVideoOutput(vw);
+    player->setMedia(QUrl::fromLocalFile("C:/Users/HP/Desktop/5th sem project/fifa2017analysis/introVideo.mov"));
+    vw->setGeometry(0,0,1920,1028);
+    MainWindow w;
+    vw->showFullScreen();
+    player->play();
+    Sleeper::sleep(12);
+    vw->close();
+    w.showFullScreen();
+    return a.exec();
+}
